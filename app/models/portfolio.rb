09c375468afec0ad5578abb198dab160d3443193
vikitorfg <victorgarzel@hotmail.com>
2020-10-04 09:31:04 -0300

    feature: set default value for main_image and thumb_image for portfolios model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,11 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :pythondjango, -> { where(subtitle: 'Python Django') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'https://baconmockup.com/600/400/'
+    self.thumb_image ||= 'https://baconmockup.com/300/200/'
+  end
 end
