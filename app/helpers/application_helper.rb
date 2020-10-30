f5d1148024db0aed6f6410f65e2f12a136e1ca17
vikitorfg <victorgarzel@hotmail.com>
2020-10-30 15:15:33 -0300

    refactored nav into helper, added active to app nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,32 @@ module ApplicationHelper
       content_tag(:p, greetings, class: 'source-greeting-helper')
     end
   end
+
+  def nav_items
+    [
+      { url: root_path,
+        title: 'Home' },
+      { url: contact_path,
+        title: 'Contact' },
+      { url: about_me_path,
+        title: 'About me' },
+      { url: blogs_path,
+        title: 'Blog' },
+      { url: portfolios_path,
+        title: 'Portfolio' }
+    ]
+  end
+
+  def nav_helper(styles, tag)
+    nav_string = ''
+    nav_items.each do |item|
+      nav_string << "<#{tag}><a href='#{item[:url]}' class='#{styles} #{is_active? item[:url]}'>#{item[:title]}</a></#{tag}>"
+    end
+
+    nav_string.html_safe
+  end
+
+  def is_active?(path)
+    'active' if current_page? path
+  end
 end
