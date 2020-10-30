d3b57901a66ce99bc762610dca59c8c01eebbdc9
vikitorfg <victorgarzel@hotmail.com>
2020-10-30 16:11:07 -0300

    feat modified portfolio controler to include attr for possition

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@ class PortfoliosController < ApplicationController
   access all: %i[show index pythondjango], user: { except: %i[destroy new create update edit] }, site_admin: :all
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def pythondjango
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -14,4 +14,9 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
     self.thumb_image ||= Placeholder.image_generator(height: 300, width: 200)
   end
+
+
+  def self.by_position
+    order("position ASC")
+  end
 end
