cf32205645dc0951666e50a06ae6d2abe6ff5b61
vikitorfg <victorgarzel@hotmail.com>
2020-11-06 15:27:29 -0300

    bug was just uninstalled depedency, lost 2 hours. Finished with image uploading and minor css adjustments

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,12 +11,12 @@ class Portfolio < ApplicationRecord
 
   scope :pythondjango, -> { where(subtitle: 'Python Django') }
 
-  after_initialize :set_defaults
+  # after_initialize :set_defaults
 
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-    self.thumb_image ||= Placeholder.image_generator(height: 300, width: 200)
-  end
+  # def set_defaults
+  #   self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+  #   self.thumb_image ||= Placeholder.image_generator(height: 300, width: 200)
+  # end
 
   def self.by_position
     order('position ASC')
